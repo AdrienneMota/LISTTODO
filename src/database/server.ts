a403f57e489201fdd2de0989a5969f496f0ecdf8
AdrienneMota <adriennemota97@gmail.com>
2023-07-11 18:55:47 -0300

    fix: query with error in createTask Repository

--- src/database/server.ts
+++ src/database/server.ts
@@ -1,6 +1,11 @@
+import dotenv from "dotenv"
 import pkg from "pg"
 
+dotenv.config();
+
 const { Pool } = pkg
+
+console.log(process.env.DATABASE_URL)
 
 const connectiondb = new Pool({
     connectionString: process.env.DATABASE_URL,
--- src/repositories/task.respository.ts
+++ src/repositories/task.respository.ts
@@ -3,7 +3,7 @@ import { Task } from "../models/task.model.js";
 
 async function createTask(task: Task){
     await connectiondb.query(
-        "INSERT INTO task (name, descripton, classification) VALUES ($1, $2, $3)", [task.name, task.description, task.classification_id]
+        "INSERT INTO task (name, description, classification_id) VALUES ($1, $2, $3)", [task.name, task.description, task.classification_id]
     )
 }
 
